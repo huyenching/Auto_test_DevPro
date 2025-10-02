93d7bee064e2cacd4c63c520b69a40283f7e2e90
huyenching <dohuyenchinh2607@gmail.com>
2025-10-03 02:01:55 +0700

    update Bai5.java

--- src/main/java/bai5.java
+++ src/main/java/bai5.java
@@ -1,2 +1,19 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
 public class bai5 {
+    public static void main(String args[]) throws IOException, InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://demoqa.com/radio-button");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+
+
+        driver.close();
+
+    }
 }
