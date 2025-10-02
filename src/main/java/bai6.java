63740270d99e52be564a68a720e8a96d48d9e3b8
huyenching <dohuyenchinh2607@gmail.com>
2025-10-03 02:52:59 +0700

    Update bai6

---
+++ src/main/java/bai6.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class bai6 {
+    public static void main(String args[]) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://demoqa.com/webtables");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+
+        WebElement rowCierra = driver.findElement(By.xpath("//div[text()='Cierra']/following-sibling::div[3]"));
+        System.out.println("Email của Cierra la: "+ rowCierra.getText());
+
+        WebElement rowEdit = driver.findElement(By.xpath("//div[text()='Alden']/ancestor::div[@role='row']//span[@title='Edit']"));
+        rowEdit.click();
+        Thread.sleep(5000);
+
+        driver.close();
+    }
+}
